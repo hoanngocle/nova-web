0a6644cf19ea0ad1aa6270a4f1dc72ad62c949c3
hoanngocle <hoanngocle.93@gmail.com>
2022-12-21 14:37:51 +0700

    Add config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
