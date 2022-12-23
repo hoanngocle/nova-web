a8d9334c4496ce11e87b3cd4b0e43cf842869870
hoanngocle <hoanngocle.93@gmail.com>
2022-12-23 10:47:25 +0700

    Update file

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -5,31 +5,31 @@ import logo from "@src/assets/images/logo/logo.svg";
 
 //Template config options
 const themeConfig = {
-  app: {
-    appName: "Vuexy",
-    appLogoImage: logo,
-  },
-  layout: {
-    isRTL: false,
-    skin: "light", // light, dark, bordered, semi-dark
-    type: "vertical", // vertical, horizontal
-    contentWidth: "boxed", // full, boxed
-    menu: {
-      isHidden: false,
-      isCollapsed: false,
+    app: {
+        appName: "Nova Online",
+        appLogoImage: logo,
     },
-    navbar: {
-      // ? For horizontal menu, navbar type will work for navMenu type
-      type: "floating", // static , sticky , floating, hidden
-      backgroundColor: "white", // BS color options [primary, success, etc]
+    layout: {
+        isRTL: false,
+        skin: "light", // light, dark, bordered, semi-dark
+        type: "vertical", // vertical, horizontal
+        contentWidth: "boxed", // full, boxed
+        menu: {
+            isHidden: false,
+            isCollapsed: false,
+        },
+        navbar: {
+            // ? For horizontal menu, navbar type will work for navMenu type
+            type: "floating", // static , sticky , floating, hidden
+            backgroundColor: "white", // BS color options [primary, success, etc]
+        },
+        footer: {
+            type: "static", // static, sticky, hidden
+        },
+        customizer: false,
+        scrollTop: true, // Enable scroll to top button
+        toastPosition: "top-right", // top-left, top-center, top-right, bottom-left, bottom-center, bottom-right
     },
-    footer: {
-      type: "static", // static, sticky, hidden
-    },
-    customizer: false,
-    scrollTop: true, // Enable scroll to top button
-    toastPosition: "top-right", // top-left, top-center, top-right, bottom-left, bottom-center, bottom-right
-  },
 };
 
 export default themeConfig;
