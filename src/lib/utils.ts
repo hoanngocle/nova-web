681770ef5b6ac4ff32b3d2062cfc6dbed06314d5
hoanngocle <hoanngocle.93@gmail.com>
2023-04-11 16:43:31 +0700

    add helper check admin + super admin

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,13 @@
+import { ROLE_ADMIN, ROLE_SUPER_ADMIN } from 'src/configs/constant';
+
 export const isDevMode = () => {
     return process.env.NODE_ENV === 'development';
 };
 
-export const isSuperAdmin = () => {};
+export const isSuperAdmin = (role: number) => {
+    return role === ROLE_SUPER_ADMIN;
+};
 
-export const isAdmin = () => {};
+export const isAdmin = (role: number) => {
+    return role === ROLE_ADMIN;
+};
